cc83c1e159d64c5c5c7b3310df19c0a6c007f0ac
JhenniferJS <jhennifer@gec.inatel.br>
2021-11-25 23:35:46 -0300

    Adicionando imports

--- src/test/java/br/inatel/s206/test/ExampleRunnerTest.java
+++ src/test/java/br/inatel/s206/test/ExampleRunnerTest.java
@@ -1,5 +1,9 @@
 package br.inatel.s206.test;
 
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
 @RunWith(Karate.class)
 @KarateOptions(features= "br/inatel/s206/test/example.feature")
 class ExampleRunnerTest {
